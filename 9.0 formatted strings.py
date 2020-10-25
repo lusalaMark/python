183cf72be14961739f80d9cc26b34b0ec0e86b4b
lusalaMark <lusalamark63@gmail.com>
2020-10-25 23:23:17 +0300

    formatted Strings

---
+++ 9.0 formatted strings.py
@@ -0,0 +1,10 @@
+first = 'Mark'
+last = 'lusala'
+message = first + ' [ ' + last + ' ] is a coder'
+print(message)
+print(message)
+
+first = 'Mark'
+last = 'lusala'
+msg = f'{first}[{last}] is a coder'
+print(msg)
