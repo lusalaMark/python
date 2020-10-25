0ae4d301b1ef9dd9a994edb4503c47f7f7f0d8b1
lusalaMark <lusalamark63@gmail.com>
2020-10-25 23:20:19 +0300

    Weight conversion

---
+++ 7.0 Exercise3.py
@@ -0,0 +1,4 @@
+# Write a program that asks users their weight in pounds converts it into kilograms and print on the terminal.
+weight_lbs = input('Weight in (lbs) : ')
+weight_kgs = int (weight_lbs)*.45
+print(weight_kgs)
