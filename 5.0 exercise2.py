b130d61187d93e0f27b15a7e4bac2cdeccc4f352
lusalaMark <lusalamark63@gmail.com>
2020-10-25 23:16:57 +0300

    getting input from a user

---
+++ 5.0 exercise2.py
@@ -0,0 +1,4 @@
+# Write a program that asks the user his name and his favorite color. Then displays “mark like black”
+name = input ("What is your name? ")
+color = input ("What color do you like? ")
+print (name  + ' like '+ color)
