783108ef1ad6fc22408073ca95d209fdc07cb4d4
lusalaMark <lusalamark63@gmail.com>
2020-10-25 23:24:19 +0300

    String methods

---
+++ 10. 0 String methods.py
@@ -0,0 +1,8 @@
+course = "python for beginners"
+print(course.upper())
+
+course = "python for beginners"
+print(course.replace("p", "j"))
+
+course = "python for begginers"
+print("python" in course)
