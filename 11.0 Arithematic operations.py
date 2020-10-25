0063c0a6178666770dfd02d03d9b027f681ad52c
lusalaMark <lusalamark63@gmail.com>
2020-10-25 23:25:11 +0300

    Operations

---
+++ 11.0 Arithematic operations.py
@@ -0,0 +1,17 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3)
+print(10 // 3)
+print(10 ** 3)
+
+x = 10
+x -= 3
+print(x)
+
+# Operator precedence
+# parenthesis
+# Exponential
+# Multiplication/division
+# Addition/subtraction
