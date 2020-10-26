776ca149bc30ec78dd90a6f8112bc0555ac1f65d
lusalaMark <lusalamark63@gmail.com>
2020-10-27 02:48:40 +0300

    for loops in lists

--- 24. Lists.py
+++ 24. Lists.py
@@ -1,11 +1,24 @@
 names = ["mark", "bob", "mosh", "sarah", "mary"]
 print(names)
-numbers = [2, 5, 8, 9, 3, 5, 2, 6]
+numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(numbers)
 numbers.sort()
 print(numbers)
-
+print(numbers[2:4])
+print(numbers[5:])
+print(numbers[0:-1])
+print(numbers[0:10:2])
+print(numbers[::-1])
+print(numbers[5:0:-2])
 print(numbers[:5])  # slicing
+
+for i in range(len(numbers)):
+    print(numbers[0:i])
+window_size = 2
+
+for i in range(len(numbers)-(window_size-1)):
+    print(numbers[i:i+window_size])
+
 
 for numbers in numbers:
     print(numbers)
@@ -22,7 +35,7 @@ names = ["mark", "bob", "mosh", "sarah", "marry"]
 print(names[2:])
 
 names = ["mark", "bob", "mosh", "sarah", "marry"]
-print(names[2:4])
+
 
 numbers = [3, 5, 7, 9, 0, 4, 2]
 maximum: int = numbers[0]
