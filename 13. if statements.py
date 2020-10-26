3ca36aa48e154d0b24bb407bafe4e630068b90f3
lusalaMark <lusalamark63@gmail.com>
2020-10-26 12:59:06 +0300

    if statements

---
+++ 13. if statements.py
@@ -0,0 +1,11 @@
+is_cold=False
+is_hot = True
+if is_hot:
+    print("it is a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("it is a cold day")
+    print("Wear warm clothes")
+else:
+    print("it is a lovely day ")
+print("Enjoy your day")
--- 3.0 variables.py
+++ 3.0 variables.py
@@ -1,5 +1,17 @@
+import turtle
 price = 10
 print (price)
 rating = 4.9
 name = "mark"
+print(name)
 is_published = True
+
+# variable + turtle
+mark_turtle = turtle.Turtle()
+mark_turtle.forward(200)
+mark_turtle.right(160)
+mark_turtle.forward(200)
+mark_turtle.left(160)
+mark_turtle.backward(200)
+mark_turtle.right(340)
+mark_turtle.forward(200)
