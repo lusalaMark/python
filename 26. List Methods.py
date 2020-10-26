1b53fa6f42ceeeddc37b2b6a17fc4bd4792329ce
lusalaMark <lusalamark63@gmail.com>
2020-10-27 02:58:39 +0300

    List Methods

---
+++ 26. List Methods.py
@@ -0,0 +1,38 @@
+numbers =[2,3,5,4,7,8,9]
+numbers.append(56)
+print(numbers)
+
+numbers =[2,3,5,4,7,8,9]
+numbers.insert(0,56)
+print(numbers)
+
+numbers =[2,3,5,4,7,8,9]
+numbers.remove(5)
+print(numbers)
+
+numbers =[2,3,5,4,7,8,9]
+numbers.clear()
+print(numbers)
+
+numbers =[2,3,5,4,7,8,9]
+numbers.pop()
+print(numbers)
+
+numbers =[2,3,5,4,7,8,9]
+print(numbers.index(5))
+
+numbers =[2,3,5,4,7,8,9]
+print(4 in numbers)
+
+numbers =[2,3,5,4,7,5,5,8,9]
+print(numbers.count(5))
+
+numbers =[2,3,5,4,7,5,5,8,9]
+numbers.sort()
+print(numbers)
+
+numbers =[2,3,5,4,7,5,5,8,9]
+numbers.sort()
+numbers.reverse()
+print(numbers)
+
