35384b7cb2f16db0b40f306560aa360a8fcad635
lusalaMark <lusalamark63@gmail.com>
2020-10-27 03:31:04 +0300

    Tuples

--- 28. tuples.py
+++ 28. tuples.py
@@ -1,6 +1,10 @@
 numbers = (1, 2, 3, 4, 5)
 print(numbers)
 
-numbers = [1, 2, 3, 4, 5]
-numbers[0] = 10
-print(numbers[0])
+credit_card_no1 = (1235367, "mark Lusala", "11/23", 123)
+credit_card_no2 = (1235367, "mark Lusala", "11/23", 123)
+
+
+creditCards = credit_card_no1 + credit_card_no2
+
+print(creditCards)
