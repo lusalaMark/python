debbcdef60e8b8db1eb55992bbca4c76122f3944
lusalaMark <lusalamark63@gmail.com>
2020-10-27 20:58:03 +0300

    classes in python

---
+++ 40.Classes.py
@@ -0,0 +1,56 @@
+def draw():
+    print("draw")
+
+
+def move():
+    print("move")
+
+
+class Point:
+    pass
+
+
+point1 = Point()
+draw()
+
+
+def move():
+    print("move")
+
+
+def draw():
+    print("draw")
+
+
+class Point:
+    pass
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
+draw()
+
+
+def move():
+    print("move")
+
+
+def draw():
+    print("draw")
+
+
+class Point:
+    pass
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
+draw()
+
+point2 = Point()
+point2.x = 3
+print(point2.x)
