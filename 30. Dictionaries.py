c31908bec55e673f19f22c4b496d5d5b731c1ef8
lusalaMark <lusalamark63@gmail.com>
2020-10-27 04:02:16 +0300

    Dictionaries

---
+++ 30. Dictionaries.py
@@ -0,0 +1,22 @@
+customer ={
+    "name":"john Smith",
+    "age": 30,
+    "is verified": True
+}
+print(customer ["name"])
+
+customer ={
+    "name":"john Smith",
+    "age": 30,
+    "is verified": True
+}
+print(customer.get("dirth", "i am doing great"))
+
+customer ={
+    "name":"john Smith",
+    "age": 30,
+    "is verified": True
+}
+customer ["name"] = "sheila kiseako"
+print(customer["name"])
+
