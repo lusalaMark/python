52437e3407eb555dc397db8eaf9da60e9c52b828
lusalaMark <lusalamark63@gmail.com>
2020-10-27 14:25:31 +0300

    Exercise on Dictionaries

--- 30. Dictionaries.py
+++ 30. Dictionaries.py
@@ -1,22 +1,18 @@
-customer ={
-    "name":"john Smith",
+customer = {
+    "name": "john Smith",
     "age": 30,
     "is verified": True
 }
-print(customer ["name"])
+print(customer["name"])
 
-customer ={
-    "name":"john Smith",
+customer = {
+    "name": "john Smith",
     "age": 30,
     "is verified": True
 }
 print(customer.get("dirth", "i am doing great"))
 
-customer ={
-    "name":"john Smith",
-    "age": 30,
-    "is verified": True
-}
-customer ["name"] = "sheila kiseako"
+customer = {"name": "sheila kiseako", "age": 30, "is verified": True}
 print(customer["name"])
+
 
