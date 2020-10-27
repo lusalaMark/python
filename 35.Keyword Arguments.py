94ce93efc2803da4cacb7a19275cc52f9b416193
lusalaMark <lusalamark63@gmail.com>
2020-10-27 20:35:10 +0300

    keyword arguments

---
+++ 35.Keyword Arguments.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name}{last_name} !")
+    print("Welcome abroad !!")
+
+
+print("Start")
+greet_user(last_name="smmith", first_name="john")
+
+print("finish")
