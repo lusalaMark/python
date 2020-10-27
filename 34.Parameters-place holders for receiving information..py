51663f70f3b25ff85c9fc25266f0adcee2c1edab
lusalaMark <lusalamark63@gmail.com>
2020-10-27 20:34:00 +0300

    place holders for information

---
+++ 34.Parameters-place holders for receiving information..py
@@ -0,0 +1,19 @@
+def greet_user(name):
+    print(f"Hi {name} !")
+    print("Welcome abroad !!")
+
+
+print("Start")
+greet_user("john")
+print("finish")
+
+
+def greet_user(name):
+    print(f"Hi {name} !")
+    print("Welcome abroad !!")
+
+
+print("Start")
+greet_user("john")
+greet_user("mary")
+print("finish")
