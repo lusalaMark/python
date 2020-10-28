09a90cf4243e531c1387a77151eb249a1e85eed7
lusalaMark <lusalamark63@gmail.com>
2020-10-28 15:24:36 +0300

    packages

---
+++ 45.Packages.py
@@ -0,0 +1,24 @@
+from ecommmerce.shipping import calc_shipping
+
+calc_shipping()
+calc_shipping()
+calc_shipping()
+calc_shipping()
+calc_shipping()
+
+
+def calc_shipping():
+    print("calc_shipping")
+
+
+from ecommmerce import shipping
+
+shipping.calc_shipping()
+
+from ecommmerce import shipping
+
+shipping.calc_shipping()
+
+
+def calc_shipping():
+    print("calc_shipping")
